5eb6cccda23205c2181f780e95709f0828a70ea6
kenncula <kc874@cornell.edu>
2021-09-24 21:32:22 -0400

    Create content.py

---
+++ content.py
@@ -0,0 +1,20 @@
+class Content ():
+    def __init__(self, id, title, year, age, imdb, rotten, netflix, hulu, prime,
+                 disney, type, director, genre, country, language, runtime):
+
+        self.id = id
+        self.title = title
+        self.year = year
+        self.age = age
+        self.imdb = imdb
+        self.rotten = rotten
+        self.netflix = netflix
+        self.hulu = hulu
+        self.prime = prime
+        self.disney = disney
+        self.type = type
+        self.director = director
+        self.genre = genre
+        self.country = country
+        self.language = language
+        self.runtime = runtime
